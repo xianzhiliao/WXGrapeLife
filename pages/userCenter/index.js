b41a9b9da7bc1b575b77cb525809b2639a7bdfe7
xianzhiliao <liao_xianzhi@163.com>
2016-09-26 23:33:47 +0800

    个人中心布局完善

--- pages/userCenter/index.js
+++ pages/userCenter/index.js
@@ -8,40 +8,41 @@ Page({
     itemList: [
             {
                 id: 'order',
-                iconPath:"image/icon_order.png",
-                title: '我的订单'
+                iconPath:"/image/icon_order.png",
+                title: '我的订单',
+                navUrl:'detail'
             }, {
                 id: 'cart',
-                iconPath:"image/icon_cart.png",
-                title: '购物车'
+                iconPath:"/image/icon_cart.png",
+                title: '购物车',
+                navUrl:'detail'
             }, {
                 id: 'coupon',
-                iconPath:"image/icon_coupon.png",
-                title: '我的优惠'
+                iconPath:"/image/icon_coupon.png",
+                title: '我的优惠',
+                navUrl:'detail'
             }, {
                 id: 'feedback',
-                iconPath:"image/icon_cus.png",
-                title: '客服与反馈'
+                iconPath:"/image/icon_cus.png",
+                title: '客服与反馈',
+                navUrl:'detail'
             }, {
                 id: 'card',
-                iconPath:"image/icon_ptcard.png",
-                title: '葡萄卡'
+                iconPath:"/image/icon_ptcard.png",
+                title: '葡萄卡',
+                navUrl:'detail'
             }, {
                 id: 'more',
-                iconPath:"image/icon_more.png",
-                title: '更多'
+                iconPath:"/image/icon_more.png",
+                title: '更多',
+                navUrl:'detail'
             }, {
                 id: 'dev',
-                iconPath:"image/icon_dev.png",
-                title: '开发者中心'
+                iconPath:"/image/icon_dev.png",
+                title: '开发者中心',
+                navUrl:'detail'
             }
         ]
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: 'detail'
-    })
   },
   onLoad: function () {
     console.log('onLoad')
@@ -53,5 +54,22 @@ Page({
         userInfo:userInfo
       })
     })
+  },
+  //事件处理函数
+  avatarTap: function() {
+    wx.navigateTo({
+      url: 'detail'
+    })
+  },
+  itemTap:function(e){
+      var id = e.currentTarget.id, list = this.data.itemList;
+        for (var i = 0, len = list.length; i < len; ++i) {
+            if (list[i].id == id) {
+                wx.navigateTo({
+                    url:list[i].navUrl
+                })
+            } else {
+            }
+        }
   }
 })
